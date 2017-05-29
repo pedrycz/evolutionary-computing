eb79a6b5956105c15bc6356406bf83661dccda34
lajonss <gfs.b16@gmail.com>
2017-05-29 22:30:57 +0200

    code snippets for future reference

--- src/main/java/pl/edu/agh/toik/ec/MainApp.java
+++ src/main/java/pl/edu/agh/toik/ec/MainApp.java
@@ -1,14 +1,41 @@
 package pl.edu.agh.toik.ec;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
 import pl.edu.agh.toik.ec.starter.Starter;
 
+// TODO make configuration pluggable again 
+@SpringBootApplication
+@ImportResource("classpath:pl/edu/agh/toik/ec/configuration/simple_configuration.xml")
 public class MainApp {
 
-    public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(args[0]);
-        applicationContext.getBean(Starter.class).run();
-    }
+	public static void main(String[] args) {
+		ConfigurableApplicationContext bootContext = SpringApplication.run(MainApp.class, args);
+
+		// ApplicationContext applicationContext = new
+		// ClassPathXmlApplicationContext(args[0]);
+		// bootContext.setParent(applicationContext);
+		bootContext.getBean(Starter.class).run();
+		// applicationContext.getBean(Starter.class).run();
+
+		/*
+		 * ConfigurableApplicationContext ctx = new SpringApplicationBuilder()
+		 * .parent(MainApp.class) //.child(new Object[]{args[0]})
+		 * //.child(BootConfiguration.class) .application() .run(args);
+		 */
+		// ctx.getBean(Starter.class).run();
+		// SpringApplication app = new SpringApplication();
+		/*
+		 * app.addInitializers(new ApplicationContextInitializer() {
+		 * 
+		 * @Override public void initialize(ConfigurableApplicationContext
+		 * applicationContext) { //TODO
+		 * 
+		 * }});
+		 */
+	}
 
 }
