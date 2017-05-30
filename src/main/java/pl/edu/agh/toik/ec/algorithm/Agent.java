46d2f9b4a32b646f85a04852768a8c28df002d24
Stawicki <stawickipiotr94@gmail.com>
2017-05-30 12:29:06 +0200

    Worker integration fixes

--- src/main/java/pl/edu/agh/toik/ec/algorithm/Agent.java
+++ src/main/java/pl/edu/agh/toik/ec/algorithm/Agent.java
@@ -1,164 +1,60 @@
 package pl.edu.agh.toik.ec.algorithm;
 
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.function.ToDoubleFunction;
-import java.util.stream.DoubleStream;
 
 import pl.edu.agh.toik.ec.algorithm.generation.PopulationGenerationStrategy;
 import pl.edu.agh.toik.ec.communication.Message;
 
-public class Agent {
+public interface Agent {
 
-    private List<Individual> population;
-    private LinkedList<AlgorithmStep> algorithmSteps = new LinkedList<>();
+    public void setup();
 
-    private LinkedList<Message> incomingMessages = new LinkedList<>();
-    private LinkedList<Message> outgoingMessages = new LinkedList<>();
+    public void makeStep();
 
-    private Property<Double> bestFitnessProperty = new Property<>();
-    private Property<Double> worstFitnessProperty = new Property<>();
-    private Property<Double> populationDiversityProperty = new Property<>();
+    public void clearSteps();
 
-    private String name = null;
+    public boolean removeStep(AlgorithmStep step);
 
-    private List<Agent> neighbours;
+    public void addStep(AlgorithmStep step);
 
-    private int populationSize;
-    private int populationDimension;
+    public void addStep(int index, AlgorithmStep step);
 
-    private PopulationGenerationStrategy populationGenerationStrategy;
+    public void receiveMessage(Message message);
 
-    private boolean initialized = false;
+    public void sendMessage(String address, Message message);
 
-    private final static ToDoubleFunction<Individual> INDIVIDUAL_TO_FITNESS_FUNCTION = new ToDoubleFunction<Individual>() {
+    public List<Message> getOutgoingMessages();
 
-        @Override
-        public double applyAsDouble(Individual value) {
-            return value.getFitness();
-        }
-    };
-    
-    public void setup() {
-        assert populationGenerationStrategy != null : "Population generation strategy cannot be null";
-        population = populationGenerationStrategy.generatePopulation(this);
-        assert population != null : "Setup population cannot be null";
-        initialized = true;
-    }
+    public boolean consumeOutgoingMessage(Message message);
 
-    public void step() {
-        assert initialized : "Agent has to be initialized first";
-        for (AlgorithmStep step : algorithmSteps)
-            step.process(this, population);
-        calculatePopulationDiversity();
-        calculateFitnessProperties();
-    }
-    
-    private void calculateFitnessProperties() {
-        DoubleStream populationFitnessStream = population.stream().mapToDouble(INDIVIDUAL_TO_FITNESS_FUNCTION);
-        bestFitnessProperty.setValue(populationFitnessStream.max().orElseGet(null));
-        worstFitnessProperty.setValue(populationFitnessStream.min().orElseGet(null));
-    }
+    public List<Message> getIncomingMessages();
 
-    private void calculatePopulationDiversity() {
-        // TODO Auto-generated method stub
+    public boolean consumeIncomingMessage(Message message);
 
-    }
+    public void setBestFitnessProperty(Property<Double> bestFitnessProperty);
 
-    public void clearSteps() {
-        algorithmSteps.clear();
-    }
+    public void setWorstFitnessProperty(Property<Double> worstFitnessProperty);
 
-    public boolean removeStep(AlgorithmStep step) {
-        assert step != null : "Algorithm step cannot be null";
-        return algorithmSteps.removeAll(Collections.singleton(step));
-    }
+    public void setPopulationDiversityProperty(Property<Double> populationDiversityProperty);
 
-    public void addStep(AlgorithmStep step) {
-        assert step != null : "Algorithm step cannot be null";
-        algorithmSteps.add(step);
-    }
+    public String getName();
 
-    public void addStep(int index, AlgorithmStep step) {
-        assert step != null : "Algorithm step cannot be null";
-        algorithmSteps.add(index, step);
-    }
+    public void setName(String name);
 
-    public void receiveMessage(Message message) {
-        incomingMessages.add(message);
-    }
+    public List<Agent> getNeighbours();
 
-    public void sendMessage(String address, Message message) {
-        outgoingMessages.add(message);
-    }
+    public void setNeighbours(List<Agent> neighbours);
 
-    public List<Message> getOutgoingMessages() {
-        return Collections.unmodifiableList(outgoingMessages);
-    }
+    public int getPopulationSize();
 
-    public boolean consumeOutgoingMessage(Message message) {
-        return outgoingMessages.remove(message);
-    }
+    public void setPopulationSize(int populationSize);
 
-    public List<Message> getIncomingMessages() {
-        return Collections.unmodifiableList(incomingMessages);
-    }
+    public int getPopulationDimension();
 
-    public boolean consumeIncomingMessage(Message message) {
-        return incomingMessages.remove(message);
-    }
+    public void setPopulationDimension(int populationDimension);
 
-    public void setBestFitnessProperty(Property<Double> bestFitnessProperty) {
-        this.bestFitnessProperty = bestFitnessProperty;
-    }
-    
-    public void setWorstFitnessProperty(Property<Double> worstFitnessProperty) {
-        this.worstFitnessProperty = worstFitnessProperty;
-    }
+    public PopulationGenerationStrategy getPopulationGenerationStrategy();
 
-    public void setPopulationDiversityProperty(Property<Double> populationDiversityProperty) {
-        this.populationDiversityProperty = populationDiversityProperty;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Agent> getNeighbours() {
-        return neighbours;
-    }
-
-    public void setNeighbours(List<Agent> neighbours) {
-        this.neighbours = neighbours;
-    }
-
-    public int getPopulationSize() {
-        return populationSize;
-    }
-
-    public void setPopulationSize(int populationSize) {
-        this.populationSize = populationSize;
-    }
-
-    public int getPopulationDimension() {
-        return populationDimension;
-    }
-
-    public void setPopulationDimension(int populationDimension) {
-        this.populationDimension = populationDimension;
-    }
-
-    public PopulationGenerationStrategy getPopulationGenerationStrategy() {
-        return populationGenerationStrategy;
-    }
-
-    public void setPopulationGenerationStrategy(PopulationGenerationStrategy populationGenerationStrategy) {
-        this.populationGenerationStrategy = populationGenerationStrategy;
-    }
+    public void setPopulationGenerationStrategy(PopulationGenerationStrategy populationGenerationStrategy);
 
 }
--- src/main/java/pl/edu/agh/toik/ec/algorithm/AgentImpl.java
+++ src/main/java/pl/edu/agh/toik/ec/algorithm/AgentImpl.java
@@ -1,47 +1,190 @@
 package pl.edu.agh.toik.ec.algorithm;
 
+import pl.edu.agh.toik.ec.algorithm.generation.PopulationGenerationStrategy;
 import pl.edu.agh.toik.ec.communication.Message;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.DoubleStream;
 
 public class AgentImpl implements Agent {
 
+    private List<Individual> population;
+    private LinkedList<AlgorithmStep> algorithmSteps = new LinkedList<>();
 
-    //temporary stub for neighbours - maybe just an array of names?
-    private ArrayList<String> neighbours;
-    private String name;
+    private LinkedList<Message> incomingMessages = new LinkedList<>();
+    private LinkedList<Message> outgoingMessages = new LinkedList<>();
 
-    public AgentImpl() {
-        this.neighbours = new ArrayList();
+    private Property<Double> bestFitnessProperty = new Property<>();
+    private Property<Double> worstFitnessProperty = new Property<>();
+    private Property<Double> populationDiversityProperty = new Property<>();
+
+    private String name = null;
+
+    private List<Agent> neighbours;
+
+    private int populationSize;
+    private int populationDimension;
+
+    private PopulationGenerationStrategy populationGenerationStrategy;
+
+    private boolean initialized = false;
+
+    private final static ToDoubleFunction<Individual> INDIVIDUAL_TO_FITNESS_FUNCTION = new ToDoubleFunction<Individual>() {
+
+        @Override
+        public double applyAsDouble(Individual value) {
+            return value.getFitness();
+        }
+    };
+    
+    @Override
+    public void setup() {
+        assert populationGenerationStrategy != null : "Population generation strategy cannot be null";
+        population = populationGenerationStrategy.generatePopulation(this);
+        assert population != null : "Setup population cannot be null";
+        initialized = true;
     }
 
-    public  ArrayList getNeighbours() {
-        return neighbours;
+    @Override
+    public void makeStep() {
+        assert initialized : "Agent has to be initialized first";
+        for (AlgorithmStep step : algorithmSteps)
+            step.process(this, population);
+        calculatePopulationDiversity();
+        calculateFitnessProperties();
+    }
+    
+    private void calculateFitnessProperties() {
+        DoubleStream populationFitnessStream = population.stream().mapToDouble(INDIVIDUAL_TO_FITNESS_FUNCTION);
+        bestFitnessProperty.setValue(populationFitnessStream.max().orElseGet(null));
+        worstFitnessProperty.setValue(populationFitnessStream.min().orElseGet(null));
     }
 
-    public void setNeighbours(ArrayList neighbours) {
-        this.neighbours = neighbours;
+    private void calculatePopulationDiversity() {
+        // TODO Auto-generated method stub
+
+    }
+    
+    @Override
+    public void clearSteps() {
+        algorithmSteps.clear();
     }
 
+    @Override
+    public boolean removeStep(AlgorithmStep step) {
+        assert step != null : "Algorithm step cannot be null";
+        return algorithmSteps.removeAll(Collections.singleton(step));
+    }
+
+    @Override
+    public void addStep(AlgorithmStep step) {
+        assert step != null : "Algorithm step cannot be null";
+        algorithmSteps.add(step);
+    }
+    
+    @Override
+    public void addStep(int index, AlgorithmStep step) {
+        assert step != null : "Algorithm step cannot be null";
+        algorithmSteps.add(index, step);
+    }
+    
+    @Override
+    public void receiveMessage(Message message) {
+        incomingMessages.add(message);
+    }
+    
+    @Override
+    public void sendMessage(String address, Message message) {
+        outgoingMessages.add(message);
+    }
+    
+    @Override
+    public List<Message> getOutgoingMessages() {
+        return Collections.unmodifiableList(outgoingMessages);
+    }
+    
+    @Override
+    public boolean consumeOutgoingMessage(Message message) {
+        return outgoingMessages.remove(message);
+    }
+    
+    @Override
+    public List<Message> getIncomingMessages() {
+        return Collections.unmodifiableList(incomingMessages);
+    }
+    
+    @Override
+    public boolean consumeIncomingMessage(Message message) {
+        return incomingMessages.remove(message);
+    }
+    
+    @Override
+    public void setBestFitnessProperty(Property<Double> bestFitnessProperty) {
+        this.bestFitnessProperty = bestFitnessProperty;
+    }
+    
+    @Override   
+    public void setWorstFitnessProperty(Property<Double> worstFitnessProperty) {
+        this.worstFitnessProperty = worstFitnessProperty;
+    }
+    
+    @Override
+    public void setPopulationDiversityProperty(Property<Double> populationDiversityProperty) {
+        this.populationDiversityProperty = populationDiversityProperty;
+    }
+    
+    @Override
     public String getName() {
         return name;
     }
-
+    
+    @Override
     public void setName(String name) {
         this.name = name;
     }
-
-
-    public void makeStep() {
-        System.out.println("doing some steps...");
+    
+    @Override
+    public List<Agent> getNeighbours() {
+        return neighbours;
+    }
+    
+    @Override
+    public void setNeighbours(List<Agent> neighbours) {
+        this.neighbours = neighbours;
+    }
+    
+    @Override
+    public int getPopulationSize() {
+        return populationSize;
+    }
+    
+    @Override
+    public void setPopulationSize(int populationSize) {
+        this.populationSize = populationSize;
+    }
+    
+    @Override
+    public int getPopulationDimension() {
+        return populationDimension;
+    }
+    
+    @Override
+    public void setPopulationDimension(int populationDimension) {
+        this.populationDimension = populationDimension;
+    }
+    
+    @Override
+    public PopulationGenerationStrategy getPopulationGenerationStrategy() {
+        return populationGenerationStrategy;
+    }
+    
+    @Override
+    public void setPopulationGenerationStrategy(PopulationGenerationStrategy populationGenerationStrategy) {
+        this.populationGenerationStrategy = populationGenerationStrategy;
     }
 
-
-    public void receiveMessage(Message message) {
-
-    }
-
-    public void sendMessage(Message msg) {
-
-    }
 }
