47d5659001e7f4a2f5dc2a20e31f4e7fa4ee3474
ppedrycz <piotrekpedrycz@gmail.com>
2017-05-30 11:50:17 +0200

    Config configurable again

--- src/main/java/pl/edu/agh/toik/ec/MainApp.java
+++ src/main/java/pl/edu/agh/toik/ec/MainApp.java
@@ -3,17 +3,15 @@ package pl.edu.agh.toik.ec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 import pl.edu.agh.toik.ec.starter.Starter;
 
-// TODO make configuration pluggable again 
 @SpringBootApplication
-@ImportResource("classpath:pl/edu/agh/toik/ec/configuration/simple_configuration.xml")
 public class MainApp {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext bootContext = SpringApplication.run(MainApp.class, args);
+		ConfigurableApplicationContext bootContext = SpringApplication.run(new Object[] {
+                MainApp.class, "classpath:" + args[0]}, args);
 		bootContext.getBean(Starter.class).run();
 	}
 
