123fa5d3b9ff2cc736a6cad2c747807d1b5e09bb
baran <jakub.baranski95@gmail.com>
2017-06-04 14:10:06 +0200

    AgentConfiguration modified

--- src/main/java/pl/edu/agh/toik/ec/configuration/AgentConfiguration.java
+++ src/main/java/pl/edu/agh/toik/ec/configuration/AgentConfiguration.java
@@ -1,7 +1,9 @@
 package pl.edu.agh.toik.ec.configuration;
 
+import pl.edu.agh.toik.ec.algorithm.AlgorithmStep;
 import pl.edu.agh.toik.ec.algorithm.generation.PopulationGenerationStrategy;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,12 +15,14 @@ public class AgentConfiguration {
     private final int populationDimension;
     private final PopulationGenerationStrategy populationGenerationStrategy;
     private final Map<AgentParameter, PropertyConfiguration> parameterConfiguration;
+    private final List<AlgorithmStep> algorithmSteps;
 
-    public AgentConfiguration(int populationSize, int populationDimension, PopulationGenerationStrategy populationGenerationStrategy, Map<AgentParameter, PropertyConfiguration> parameterConfiguration) {
+    public AgentConfiguration(int populationSize, int populationDimension, PopulationGenerationStrategy populationGenerationStrategy, Map<AgentParameter, PropertyConfiguration> parameterConfiguration, List<AlgorithmStep> algorithmSteps) {
         this.populationSize = populationSize;
         this.populationDimension = populationDimension;
         this.populationGenerationStrategy = populationGenerationStrategy;
         this.parameterConfiguration = parameterConfiguration;
+        this.algorithmSteps = algorithmSteps;
     }
 
     public int getPopulationSize() {
@@ -35,5 +39,9 @@ public class AgentConfiguration {
 
     public Map<AgentParameter, PropertyConfiguration> getParameterConfiguration() {
         return parameterConfiguration;
+    }
+
+    public List<AlgorithmStep> getAlgorithmSteps() {
+        return algorithmSteps;
     }
 }
