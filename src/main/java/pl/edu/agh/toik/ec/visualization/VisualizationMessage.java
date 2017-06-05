d16ce85edcf09a219dad5cbbf59e4de764649669
lajonss <gfs.b16@gmail.com>
2017-06-05 20:38:51 +0200

    basic info hook

--- src/main/java/pl/edu/agh/toik/ec/visualization/VisualizationMessage.java
+++ src/main/java/pl/edu/agh/toik/ec/visualization/VisualizationMessage.java
@@ -5,15 +5,15 @@ import java.util.Random;
 import pl.edu.agh.toik.ec.communication.Message;
 
 public class VisualizationMessage {
-	public String workerId;
-	public int timestamp;
-	public double fitness;
+    public String workerId;
+    public int timestamp;
+    public double fitness;
 
-	public VisualizationMessage(Message rawData) {
-		// TODO implement
-		Random random = new Random();
-		workerId = "" + random.nextInt(4);
-		timestamp = (int) (System.currentTimeMillis() / 1000);
-		fitness = random.nextDouble();
-	}
+    public VisualizationMessage(Message rawData) {
+	workerId = rawData.getWorkerName();
+	// TODO implement
+	Random random = new Random();
+	timestamp = (int) (System.currentTimeMillis() / 1000);
+	fitness = random.nextDouble();
+    }
 }
