15da24b7567a6b0e57cb4c8e13b7e6d5bf6fb915
lajonss <gfs.b16@gmail.com>
2017-06-06 01:46:06 +0200

    missing class

---
+++ src/main/java/pl/edu/agh/toik/ec/visualization/VisualizationMessageParseException.java
@@ -0,0 +1,6 @@
+package pl.edu.agh.toik.ec.visualization;
+
+//TODO remove this class when it is no longer useful
+public class VisualizationMessageParseException extends Exception {
+	private static final long serialVersionUID = 8156508738531981216L;
+}
