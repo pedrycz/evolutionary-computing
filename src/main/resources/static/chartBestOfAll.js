37b3d624b398e995b5e5690372ad881153fad46f
haz111 <haz111pl@gmail.com>
2017-06-06 11:18:59 +0200

    pasted visualization frontend files

---
+++ src/main/resources/static/chartBestOfAll.js
@@ -0,0 +1,38 @@
+var chartBestOfAll = Highcharts.chart('chartBestOfAll', {
+
+    chart: {
+        type: 'bar'
+    },
+
+    title: {
+        text: 'Best fitness of all workers'
+    },
+
+    xAxis: {
+        categories: ['Worker 0', 'Worker 1', 'Worker 2', 'Worker 3']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Fitness of worker'
+        }
+    },
+    legend: {
+        reversed: true
+    },
+
+    plotOptions: {
+        line: {
+            enableMouseTracking: false
+        },
+
+        series:{
+            pointInterval: 86400000
+        }
+    },
+
+    series: [{
+        data: []
+    }]
+
+});
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,54 +1,103 @@
-(function() {
-    function log(whatever) { console.log(whatever); }
-    function ajax(callback, path) {
-	var xhr;
-	if (window.ActiveXObject) {
-            try {
-		xhr = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch(e) {
-		alert(e.message);
-		xhr = null;
-            }
-	} else {
-            xhr = new XMLHttpRequest();
-	}
-	xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-		callback(xhr);
-            }
-	}
-	xhr.open('GET', path, true);
-	//xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.send();
-	return xhr;
+(function () {
+    function log(whatever) {
+        console.log(whatever);
     }
 
-    
-    window.addEventListener('load', function() {
-	var fetch = document.getElementById('fetch');
+    function ajax(callback, path) {
+        var xhr;
+        if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert(e.message);
+                xhr = null;
+            }
+        } else {
+            xhr = new XMLHttpRequest();
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                callback(xhr);
+            }
+        };
+        xhr.open('GET', path, true);
+        //xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send();
+        return xhr;
+    }
+
+    function changeVisualization(name) {
+        console.log("change vis " + name);
+        for (var i = 0; i<visualizations.length; i++) {
+            document.getElementById(visualizations[i]).style.display = (visualizations[i] === name ? "" : "none");
+        }
+    }
+
+    function setConfigLogType(name) {
+        if (name === "AllInTime")
+            changeVisualization("chartAllInTime");
+        else if (name === "BestInTime")
+            changeVisualization("chartBestInTime");
+        else if (name === "BestOfAll")
+            changeVisualization("chartBestOfAll");
+        else if (name === "Table")
+            changeVisualization("tableOutput");
+        else if (name === "LogType")
+            changeVisualization("ws-output");
+    }
+
+
+    window.addEventListener('load', function () {
+        var fetch = document.getElementById('fetch');
         var output = document.getElementById('output');
 
-	fetch.addEventListener('click', function() {
-	    ajax(function(xhr) {
-		output.innerHTML = xhr.response;
+        ajax(function (xhr) {
+            var config = JSON.parse(xhr.response);
+            setConfigLogType(config.type);
+        }, 'http://localhost:8080/config');
+
+        fetch.addEventListener('click', function () {
+            ajax(function (xhr) {
+                output.innerHTML = xhr.response;
             }, 'http://127.0.0.1:8080/type');
         });
 
         var wsOutput = document.getElementById('ws-output');
 
-	(function() {
-        var socket = new SockJS('/websocket');
-	    var client = Stomp.over(socket);
-	    client.connect({}, function(frame) {
-		console.log("Connected to server");
-		console.log(frame);
-		client.subscribe('/fitness', function(msg) {
-		    console.log("Received from server");
-		    var info = JSON.parse(msg.body);
-		    console.log(info);
-		    wsOutput.innerHTML = msg.body + '<br />' +  wsOutput.innerHTML;
-		});
-	    });
-	})();
+        (function () {
+            var socket = new SockJS('/websocket');
+            var client = Stomp.over(socket);
+            client.connect({}, function (frame) {
+                console.log("Connected to server");
+                console.log(frame);
+                client.subscribe('/fitness', function (msg) {
+                    // console.log("Received from server");
+                    var info = JSON.parse(msg.body);
+                    // console.log(info);
+                    addPointToCharts(info.workerId, info.timestamp, info.fitness);
+
+                    wsOutput.innerHTML = msg.body + '<br />' + wsOutput.innerHTML;
+                });
+            });
+        })();
+
+
+        document.getElementById("buttonAllInTime").addEventListener("click", function() {
+            changeVisualization("chartAllInTime");
+        });
+        document.getElementById("buttonBestInTime").addEventListener("click", function() {
+            changeVisualization("chartBestInTime");
+        });
+        document.getElementById("buttonBestOfAll").addEventListener("click", function() {
+            changeVisualization("chartBestOfAll");
+        });
+        document.getElementById("buttonTable").addEventListener("click", function() {
+            changeVisualization("tableOutput");
+        });
+        document.getElementById("buttonLogs").addEventListener("click", function() {
+            changeVisualization("ws-output");
+        });
     });
+
+
 })();
